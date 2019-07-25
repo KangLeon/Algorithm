6dc15fc784333138795009b8cc8cb97a8fb3b203
Leon Kang <kangleonjy@gmail.com>
2019-07-25 17:52:26 +0800

    new
    new

--- InsertonSort/InsertonSort/main.cpp
+++ InsertonSort/InsertonSort/main.cpp
@@ -14,24 +14,27 @@ using namespace std;
 
 template<typename T>
 
-//插入排序
+//插入排序：特点可以终端内层循环
 void insertionSort(T arr[], int n){
     //因为第0个位置的数字相当于已经进行过插入排序了
     for (int i=1; i<n; i++) {
         //寻找元素arr[i]合适的插入位置
-        for (int j=i; j>0&&arr[j]<arr[j-1]; j--) {
-            swap(arr[j], arr[j-1]);
+        T e=arr[i];
+        int j;//j保存元素e应该插入的位置
+        for (j=i; j>0&&arr[j-1]>e; j--) {
+            arr[j]=arr[j-1];
         }
+        arr[j]=e;
     }
 }
 
 int main(int argc, const char * argv[]) {
     int n=10000;
     int *arr=SortTestHelper::generateRandomArray(n, 0, n);
-    insertionSort(arr, n);
-    SortTestHelper::printArray(arr, n);
+//    insertionSort(arr, n);
+//    SortTestHelper::printArray(arr, n);
     
-    SortTestHelper::testSort("Selection Sort", insertionSort, arr, n);
+    SortTestHelper::testSort("insertionSort", insertionSort, arr, n);
     
     delete [] arr;
     
