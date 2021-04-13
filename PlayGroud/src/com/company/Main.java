d3b144c81d6d12fec8609384a031934cd3768675
jitengjiao <15048471585@163.com>
2021-04-13 13:45:25 +0800

    new
    new

---
+++ PlayGroud/src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+
+    private Stack<Integer> inStack = new Stack<>();
+    private Stack<Integer> outStack = new Stack<>();
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        checkOutStack();
+
+        return outStack.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        checkOutStack();
+
+        return outStack.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return inStack.isEmpty()&&outStack.isEmpty();
+    }
+
+    public void checkOutStack() {
+        if(outStack.isEmpty()){
+            while (!inStack.isEmpty()){
+                outSta ck.push(inStack.pop());
+            }
+        }
+    }
+}
