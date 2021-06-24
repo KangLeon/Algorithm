f212dbdec7b218673d1ab7a428edaa94e19c047a
jitengjiao <15048471585@163.com>
2021-06-24 13:30:30 +0800

    new
    new

--- 2021-06-16/src/com/company/sort/QuickSort.java
+++ 2021-06-16/src/com/company/sort/QuickSort.java
@@ -3,8 +3,35 @@ package com.company.sort;
 public class QuickSort<E extends Comparable<E>> extends Sort<E>  {
     @Override
     protected void sort() {
-
+        sort(0,array.length);
     }
 
-    
+    /*
+    * 对[begin,end)范围的元素进行快速排序
+    * @param begin
+    * @param end
+    * */
+    private void sort(int begin,int end){
+        if (end - begin < 2) return;
+        //确定轴点位置
+        int mid = privotIndex(begin,end);
+        //对子序列进行快速排序
+        sort(begin,end);
+        sort(mid+1,end);
+    }
+
+    /*
+    * 构造出[begin,end）范围的轴点元素
+    * @return轴点元素的最终位置
+    * */
+    private int privotIndex(int begin,int end){
+        //备份begin位置的元素
+        E v = array[begin];
+        //end指向最后一个元素
+        end--;
+
+        while (begin < end){
+
+        }
+    }
 }
