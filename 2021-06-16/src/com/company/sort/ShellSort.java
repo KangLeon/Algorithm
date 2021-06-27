2aa292bcfe5786e7da9ca7b3ddd0aa87e76aa859
jitengjiao <15048471585@163.com>
2021-06-27 15:23:30 +0800

    希尔排序的另一个科学家提供的确定步长序列的方法
    希尔排序的另一个科学家提供的确定步长序列的方法

--- 2021-06-16/src/com/company/sort/ShellSort.java
+++ 2021-06-16/src/com/company/sort/ShellSort.java
@@ -1,6 +1,7 @@
 package com.company.sort;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ShellSort<E extends Comparable<E>> extends Sort<E> {
@@ -9,7 +10,7 @@ public class ShellSort<E extends Comparable<E>> extends Sort<E> {
     protected void sort() {
         List<Integer> stepSequence = shellStepSquence();
         for(Integer step : stepSequence){
-
+            sort(step);
         }
     }
 
@@ -18,9 +19,27 @@ public class ShellSort<E extends Comparable<E>> extends Sort<E> {
     * @param step
     * */
     private void sort(int step){
-
+        //col：第几列，column的简称
+        for (int col = 0; col < step; col++) {//对第col列进行排序
+            //col、col+step，col+2*step、col+3*step
+            for (int begin = col + step; begin < array.length; begin+=step) {
+                int cur = begin;
+                while (cur > col && cmp(cur,cur-step) < 0){
+                    swap(cur,cur-step);
+                    cur -= step;
+                }
+            }
+        }
     }
-    
+
+    protected void swap(int v1,int v2){
+        int tmp = v1;
+        v1 = v2;
+        v2 = tmp;
+    }
+
+
+    //希尔提供的确定步长序列
     private List<Integer> shellStepSquence() {
         List<Integer> stepSequence = new ArrayList<>();
         int step = array.length;
@@ -31,7 +50,23 @@ public class ShellSort<E extends Comparable<E>> extends Sort<E> {
         return stepSequence;
     }
 
+    //另外一个科学家提供的确定步长序列
     private List<Integer> myStepSequence() {
-
+        List<Integer> stepSequence = new LinkedList<>();
+        int k = 0,step = 0;
+        while (true){
+            if ( k % 2 == 0){
+                int pow = (int)Math.pow(2,k>>1);
+                step = 1 + 9*(pow*pow - pow);
+            }else {
+                int pow1 = (int)Math.pow(2,(k-1)>>1);
+                int pow2 = (int)Math.pow(2,(k+1)>>1);
+                step = 1 + 8*pow1*pow2 - 6 *pow2;
+            }
+            if (step >= array.length) break;
+            stepSequence.add(0,step);
+            k++;
+        }
+        return stepSequence;
     }
 }
