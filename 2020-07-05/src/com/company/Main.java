4436d9079872358c05f84914af72b170476197de
jitengjiao <15048471585@163.com>
2021-07-05 14:59:43 +0800

    并查集的一种思路

--- 2020-07-05/src/com/company/Main.java
+++ 2020-07-05/src/com/company/Main.java
@@ -1,7 +1,31 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-05 13:21:27
+ * @LastEditTime: 2021-07-05 14:50:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /2020-07-05/src/com/company/main.java
+ */
 package com.company;
+
+import com.company.union.Unionfind;
 
 public class Main {
     public static void main(String[] args) {
-	
+        Unionfind uf = new Unionfind(12);
+        uf.union(0, 1);
+        uf.union(0, 3);
+        uf.union(0, 4);
+        uf.union(0, 3);
+        uf.union(0, 5);
+
+        uf.union(6, 7);
+
+        uf.union(8, 10);
+        uf.union(9, 10);
+        uf.union(9, 11);
+
+        System.out.println(uf.isSame(0, 6));
+        System.out.println(uf.isSame(0, 5));
     }
 }
--- 2020-07-05/src/com/company/union/Unionfind.java
+++ 2020-07-05/src/com/company/union/Unionfind.java
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-05 13:29:12
- * @LastEditTime: 2021-07-05 13:54:37
+ * @LastEditTime: 2021-07-05 14:43:35
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /2020-07-05/src/com/company/union/Unionfind.java
@@ -31,6 +31,28 @@ public class Unionfind {
         rangeCheck(v);
 
         return parents[v];
+    }
+
+    public void union(int v1,int v2){
+        int p1 = find(v1);
+        int p2 = find(v2);
+        if(p1==p2)return;
+
+        for (int i = 0; i < parents.length; i++) {
+            if(parents[i] == p1){
+                parents[i] = p2;
+            }
+        }
+    }
+
+    /**
+     * @description: 检查v1、v2是否属于同一个集合
+     * @param {int} v1
+     * @param {int} v2
+     * @return {*}
+     */    
+    public boolean isSame(int v1,int v2){
+        return find(v1) == find(v2);
     }
 
     private void rangeCheck(int v){
