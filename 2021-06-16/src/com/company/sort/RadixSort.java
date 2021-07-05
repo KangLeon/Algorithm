db0ff98aeddc46b8d389db712f68bcaf6715d699
jitengjiao <15048471585@163.com>
2021-07-05 10:52:44 +0800

    基数排序
    基数排序

--- 2021-06-16/src/com/company/sort/RadixSort.java
+++ 2021-06-16/src/com/company/sort/RadixSort.java
@@ -3,15 +3,24 @@ package com.company.sort;
 public class RadixSort extends Sort<Integer>{
     @Override
     protected void sort() {
-        
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if(array[i]>max){
+                max = array[i];
+            }
+        }
+
+        for (int divider = 0; divider < max; divider*=10) {
+            countingSort(divider);
+        }
     }
 
-    protected void countingSort() {
+    protected void countingSort(int divider) {
         //开辟内存空间，存储次数
         int[] counts = new int[10];
         //统计每个整数出现的次数
         for (int i = 0; i < array.length; i++) {
-            counts[array[i]]++;    
+            counts[array[i]/divider%10]++;    
         }
         //累加次数
         for (int i = 0; i < counts.length; i++) {
@@ -20,7 +29,7 @@ public class RadixSort extends Sort<Integer>{
         //从后往前遍历元素，将它放到有序数组中的合适位置
         int[] newArray = new int[array.length];
         for (int i = array.length - 1; i >= 0; i--) {
-            newArray[--counts[array[i]]] = array[i];
+            newArray[--counts[array[i]/divider%10]] = array[i];
         }
 
         //将有序数组赋值到array
