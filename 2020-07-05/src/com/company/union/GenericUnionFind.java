a0433c1e7702cfc43770db872179ad2919ac4e14
jitengjiao <15048471585@163.com>
2021-07-06 16:41:11 +0800

    通用的并查集方法
    可以用在普通对象上

---
+++ 2020-07-05/src/com/company/union/GenericUnionFind.java
@@ -0,0 +1,68 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-06 13:09:01
+ * @LastEditTime: 2021-07-06 14:36:46
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /2020-07-05/src/com/company/union/GenericUnionFind.java
+ */
+package com.company.union;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class GenericUnionFind<V> {
+
+    private Map<V,Node<V>> nodes = new HashMap<>();
+
+    public void makeSet(V v){
+        if(nodes.containsKey(v)) return;
+        nodes.put(v, new Node<>(v));
+    }
+
+    public Node<V> findNode(V v) {
+        Node<V> node = nodes.get(v);
+        if(node==null)return null;
+
+        while(!Objects.equals(node.value, node.parent.value)){
+            node.parent = node.parent.parent;
+            node = node.parent;
+        }
+
+        return node;
+    }
+
+    public V find(V v){
+        Node<V> node = findNode(v);
+        return node == null ? null : node.value;
+    }
+
+    public void union(V v1,V v2){
+        Node<V> p1 = findNode(v1);
+        Node<V> p2 = findNode(v2);
+        if(p1 == null || p2 == null) return;
+        if(Objects.equals(p1.value, p2.value)) return;
+        if(p1.rank < p2.rank){
+            p1.parent = p2;
+        }else if(p1.rank > p2.rank){
+            p2.parent = p1;
+        }else{
+            p1.parent = p2;
+            p2.rank += 1;
+        }
+    }
+
+    public boolean isSame(V v1,V v2){
+        return Objects.equals(find(v1), find(v2));  
+    }
+
+    private static class Node<V> {
+        V value;
+        Node<V> parent = this;
+        int rank = 1;
+        Node(V value){
+            this.value = value;
+        }
+    }
+}
---
+++ 2020-07-05/src/com/company/union/Student.java
@@ -0,0 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-06 13:40:39
+ * @LastEditTime: 2021-07-06 13:48:48
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /2020-07-05/src/com/company/union/Student.java
+ */
+package com.company.union;
+
+public class Student {
+    private int age;
+    private String name;
+    public Student(int age,String name){
+        this.age = age;
+        this.name = name;
+    }
+}
