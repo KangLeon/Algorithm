83b1df269d0a226661120fbb75f21cf1ae14d6c2
jitengjiao <15048471585@163.com>
2021-07-06 12:09:17 +0800

    并查集一种优化方法：路径压缩
    并查集一种优化方法：路径压缩

---
+++ 2020-07-05/src/com/company/union/UnionFind_QF_PC.java
@@ -0,0 +1,26 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-06 12:04:33
+ * @LastEditTime: 2021-07-06 12:08:27
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /2020-07-05/src/com/company/union/UnionFind_QF_PC.java
+ */
+package com.company.union;
+
+public class UnionFind_QF_PC extends UnionFind_QF_R {
+
+    public UnionFind_QF_PC(int capacity) {
+        super(capacity);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public int find(int v) {
+        rangeCheck(v);
+        if(parents[v]!=v){
+            parents[v] = find(parents[v]);
+        }
+        return parents[v];
+    }
+}
